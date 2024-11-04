ff080ca285ad522128b7087f2ba1f0af761547ab
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-11-04 14:17:22 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -8,6 +8,7 @@
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
 {
     BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    
   /*Switch case is reduced by replacing with function pointers*/
    void (*sendToTarget[])(BreachType breachType) = {sendToController,sendToEmail};
    sendToTarget[alertTarget](breachType);
